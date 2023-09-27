1a8a40abe58d312f89325f66a683d1e3882f0549
MarsIfeanyi <marschalice@gmail.com>
2023-09-27 11:45:24 +0100

    .

--- src/component/IdToCrowdFund.js
+++ src/component/IdToCrowdFund.js
@@ -1,38 +1,42 @@
 import React from "react";
 import useCampaign from "../hooks/useCampaign";
 import { ethers } from "ethers";
-import { shortenAccount } from "../utils";
+import { shortenAccount, formatDate } from "../utils";
 
 const IdToCrowdFund = () => {
   const allCampaigns = useCampaign();
+
+  const reversedCampaigns = [...allCampaigns].reverse();
+
   return (
     <>
-      <h2 className="text-center  text-3xl text-blue-600 mt-14 font-medium shadow-lg  ">
+      <h2 className="text-center  text-3xl text-blue-400 mt-14 font-medium shadow-lg p-4  ">
         Active and Ongoing Campaigns
       </h2>
-      <div className="grid grid-cols-3 mx-14 mt-4 gap-8 border shadow-xl  rounded-xl p-4 ">
-        {allCampaigns.map((campaign, index) => (
-          <div key={index} className="">
-            <div className="flex flex-row gap-4">
+      <div className="grid grid-cols-3 mx-14 mt-4 gap-8 border  shadow-xl  rounded-xl p-8  ">
+        {reversedCampaigns.map((campaign, index) => (
+          <div key={index} className="border-b-4 border-2 rounded-2xl p-4">
+            <div className="flex flex-row gap-2">
               <h4 className="headerStyle ">Title:</h4> {campaign.title}
             </div>
-            <div className="flex flex-row">
+            <div className="flex flex-row gap-2">
               <h4 className="headerStyle ">Funding Goal:</h4>{" "}
               {ethers.formatEther(campaign.fundingGoal)}
             </div>
-            <div className="flex flex-row">
+            <div className="flex flex-row gap-2">
               <h4 className="headerStyle ">Owner:</h4>
               {shortenAccount(campaign.owner)}
             </div>
-            <div className="flex flex-row">
+            <div className="flex flex-row gap-2">
               <h4 className="headerStyle ">Time Duration:</h4>
-              {ethers.formatUnits(campaign.durationTime)}
+
+              {formatDate(ethers.formatUnits(campaign.durationTime, 0))}
             </div>
-            <div className="flex flex-row">
+            <div className="flex flex-row gap-2">
               <h4 className="headerStyle ">Campaign Status:</h4>
               {campaign.isActive ? "Active" : "false"}
             </div>
-            <div className="flex flex-row">
+            <div className="flex flex-row gap-2">
               <h4 className="headerStyle ">Funding Balance:</h4>
               {ethers.formatEther(campaign.fundingBalance)}
             </div>
