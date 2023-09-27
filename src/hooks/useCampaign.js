3b424cdf1c873b8ae2d087d7bdb8693cfbcb3319
MarsIfeanyi <marschalice@gmail.com>
2023-09-27 08:31:43 +0100

    Create Custom Hooks

---
+++ src/hooks/useCampaign.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { contractAddress } from "../constants/ContractAddress";
+import { contractABI } from "../constants/ContractAbi";
+import { ethers } from "ethers";
+ethers;
+
+contractAddress;
+
+const useCampaign = () => {
+  return <div></div>;
+};
+
+export default useCampaign;
---
+++ src/hooks/useCreateCampaign.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { contractABI } from '../constants/ContractAbi'
+import { contractAddress } from '../constants/ContractAddress'
+
+
+const useCreateCampaign = () => {
+  return (
+    <div>useCreateCampaign</div>
+  )
+}
+
+export default useCreateCampaign
